87ac1f114bce0ab97a14648998bca1dafabda03a
fernandapasa033 <72786180+fernandapasa033@users.noreply.github.com>
2021-09-17 10:01:05 +0700

    Update Tugas_1.2 .php

--- Tugas_1.2 .php
+++ Tugas_1.2 .php
@@ -6,12 +6,9 @@
 </head>
 <body>  
 <form method="post" action="">
-    <input type="text" name="awal" placeholder="Masukkan nilai awal">
-    <br>
-    <input type="text" name="beda" placeholder="Masukkan nilai beda">
-    <br>
-    <input type="text" name="suku" placeholder="Masukkan nilai suku ke">
-    <br>
+    <input type="text" name="awal" placeholder="Masukkan nilai awal"><br>
+    <input type="text" name="beda" placeholder="Masukkan nilai beda"><br>
+    <input type="text" name="suku" placeholder="Masukkan nilai suku ke"><br>
     <input type="submit" name="submit" value="ENTER">
 </form>
 <br>
@@ -21,11 +18,9 @@
          $beda = $_POST['beda'];
          $suku = $_POST['suku'];
 $var = 0;
-
          for ($i=0; $i < $suku; $i++) {
             echo $awal += $var; 
 echo " ";
-
       $var = $beda;    
          }
       }
