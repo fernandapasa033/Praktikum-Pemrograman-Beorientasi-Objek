3a3ae30285e9649e91f65086dc2dbe5aec05eacf
fernandapasa033 <72786180+fernandapasa033@users.noreply.github.com>
2021-12-06 10:32:12 +0700

    Create Tugas_5.1 .php

---
+++ Tugas_5.1 .php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Praktikum 5</title>
+</head>
+<body>
+    <div class="container">
+        <h2>Soal 1</h2>
+
+        <!-- Program PHP -->
+        <?php
+class Pegawai
+{
+	public $nama;
+	public	function __construct($nama)
+	{
+		$this->nama = $nama;
+	}
+	public	function getNama()
+	{
+		return $this->nama;
+	}
+}
+class Manajer extends Pegawai
+{
+	public $tunjangan;
+	public	function __construct($nama, $tunjangan)
+	{
+		parent::__construct($nama);
+		$this->tunjangan = $tunjangan;
+	}
+	public	function getTunjangan()
+	{
+		return $this->tunjangan;
+	}
+}
+class Kurir extends Pegawai
+{
+	public $gaji;
+	
+
+    public	function __construct($nama, $gaji)
+	{
+		parent::__construct($nama);
+		// $this->bonus = $bonus;
+		$this->gaji = $gaji;
+
+	}
+	public	function getGaji()
+	{
+		return $this->gaji;
+	}
+}
+class Soal1
+{
+	public static
+	function Proses($peg)
+	{
+		if ($peg instanceof Manajer)
+		{
+			$man = $peg;
+			echo "<br>Nama manajer: ".$man->nama, "\n";
+			echo "<br>Tunjangan: Rp. ".strval($man->tunjangan), "\n";
+		}
+		else if ($peg instanceof Kurir)
+		{
+			$kur =  $peg;
+			echo "<br>Nama kurir: ".$kur->nama, "\n";
+			echo "<br>Gaji: Rp. ".strval($kur->gaji), "\n";
+		}
+	}
+	public static
+	function main($args)
+	{
+		echo "Atafa 20051397069", "\n";
+		$peg1 = new Manajer("Rizhaa", 120000);
+		Soal1::Proses($peg1);
+		$peg2 = new Kurir("Afataa", 250000);
+		Soal1::Proses($peg2);
+	}
+}
+Soal1::main(array());
+?>
+        <!-- END PHP  -->
+    </div>
+</body>
+</html>
