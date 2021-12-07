53adf37704a0e7bda7aae1f4db25bad27a619939
fernandapasa033 <72786180+fernandapasa033@users.noreply.github.com>
2021-12-07 11:04:50 +0700

    Add files via upload

---
+++ Tugas_6 .php/Soal 2/index.php.php
@@ -0,0 +1,41 @@
+<?php 
+    require_once 'No 2.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
+    <title>Praktikum</title>
+</head>
+
+<body>
+    <div class="container">
+        <h2 class="text-center">Praktikum</h2>
+        <div class="row">
+            <div class="col-5 mx-auto border p-3 mt-2">
+                <h4 class="text-center">Soal 2</h4>
+                <?= $superman->land(); ?> <br>
+                <?= $superman->takeOff(); ?> <br>
+                <?= $superman->fly(); ?> <br>
+                <?= $superman->leapBuilding(); ?> <br>
+                <?= $superman->stopBullet(); ?> <br>
+                <br>
+                <?= $bird->buildNest(); ?> <br>
+                <?= $bird->takeOff(); ?> <br>
+                <?= $bird->fly(); ?> <br>
+                <?= $bird->land(); ?> <br>
+                <?= $bird->layEggs(); ?> <br>
+                <br>
+                <?= $airplane->takeOff(); ?> <br>
+                <?= $airplane->fly(); ?> <br>
+                <?= $airplane->land(); ?> <br>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+
